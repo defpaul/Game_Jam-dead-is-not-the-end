3d781f7cad7333b3ba887b82f25ca50e81d4494c
busybeaver42 <joerg@angermayer.de>
2023-11-04 14:41:48 +0100

    add controls: Ghost via arraow keys (up, down, left, right) and press <k>. that force JohnDoe to follow the ghost.

--- main.py
+++ main.py
@@ -13,12 +13,43 @@ import random
 if not pygame.font: print('Fehler pygame.font Modul konnte nicht geladen werden!')
 if not pygame.mixer: print('Fehler pygame.mixer Modul konnte nicht geladen werden!')
 
-class ghost:
-  def __init__(self, name, x, y):
-    self.name = name
-    self.x = 100
-    self.y = 100
-    img = pygame.image.load("assets/Gost.png")
+class Ghost:
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x = 600
+        self.y = 300
+        self.img = pygame.image.load("assets/Gost.png")
+        self.img = pygame.transform.scale(self.img, (64, 64)) ### orig 32x32
+        self.speed = 2.8
+
+    def goLeft(self):
+        self.x = self.x - self.speed
+    def goRight(self):
+        self.x = self.x + self.speed
+    def goUp(self):
+        self.y = self.y - self.speed
+    def goDown(self):
+        self.y = self.y + self.speed
+
+class JohnDoe:
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x = 100
+        self.y = 100
+        self.img = pygame.image.load("assets/john_doe.png")
+        self.img = pygame.transform.scale(self.img, (64, 64))  ### orig 32x32
+        self.speed = 2.2
+
+    def geheZuPos(self, inx, iny):
+        if inx > self.x:
+            self.x = self.x + self.speed
+        else:
+            self.x = self.x - self.speed
+
+        if iny > self.y:
+            self.y = self.y + self.speed
+        else:
+            self.y = self.y - self.speed
 
 
 def restartGame():
@@ -40,9 +71,8 @@ def reduceRed(img):
 
 def main():
     pygame.init()
-
-
-    ghostInGame = ghost("Karl",100,100);
+    ghostInGame = Ghost("TheGhost",100,100);
+    johnDoeInGame = JohnDoe("JohnDoe", 100, 100);
 
     screenSize_x = 1600
     screenSize_y = 900
@@ -62,7 +92,7 @@ def main():
     while running:
 
         clock.tick(30) #30 FPS
-        screen.fill((0, 0, 0)) # screen-Surface mit Schwarz (RGB = 0, 0, 0) füllen.
+        screen.fill((200, 200, 200)) # screen-Surface mit Schwarz (RGB = 0, 0, 0) füllen.
 
         for event in pygame.event.get():
             #print("imgTrash01Pos_y:",imgTrash01Pos_y)
@@ -81,28 +111,38 @@ def main():
                         restartGame()
 
                     if event.key == pygame.K_SPACE:
-                        print("K_spave")
+                        print("K_space")
 
                 if event.key == pygame.K_ESCAPE:
                     pygame.event.post(pygame.event.Event(pygame.QUIT))
 
                 if event.key == pygame.K_LEFT:
                     print("K_Left")
+                    ghostInGame.goLeft()
 
                 if event.key == pygame.K_RIGHT:
                     print("K_Right")
+                    ghostInGame.goRight()
 
                 if event.key == pygame.K_UP:
                     print("K_Up")
+                    ghostInGame.goUp()
 
                 if event.key == pygame.K_DOWN:
                     print("K_Down")
+                    ghostInGame.goDown()
 
                 if event.key == pygame.K_k:
                     print("klopfKlop")
+                    johnDoeInGame.geheZuPos(ghostInGame.x, ghostInGame.y)
 
                 if event.key == pygame.K_e:
                     print("Erschrecken")
+
+            #draw
+            screen.blit(ghostInGame.img, (ghostInGame.x, ghostInGame.y))
+            screen.blit(johnDoeInGame.img, (johnDoeInGame.x, johnDoeInGame.y))
+
 
         # Inhalt von screen anzeigen.
             pygame.display.flip()
@@ -112,6 +152,5 @@ def main():
 
 if __name__ == '__main__':
     # Unsere Main-Funktion aufrufen.
-    # hm
 
     main()
